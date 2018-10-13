e8230704cb4bd2aa944a885ba17e27b39bd604af
Jeremy Peters <dineonyourdesign@gmail.com>
2018-10-13 12:14:57 -0400

    hotel links, slideshow speed

--- js/main.js
+++ js/main.js
@@ -60,7 +60,7 @@ $(document).ready(function(){
 	  slidesToScroll: 1,
 	  arrows: true,
 	  autoplay: true,
-	  autoplaySpeed: 6500,
+	  autoplaySpeed: 5000,
 	  lazyLoad: 'progressive',
 	  responsive: [
 	    {
